19706a3676371ae22cccae82a071da6665d687ab
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2023-03-03 11:17:23 -0700

    responsive

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -11,14 +11,11 @@ export default function About() {
             Pink Ranger is a dynamic three-piece rock band hailing from Denver,
             Colorado. The band, comprised of Madisen Kwon, Sean Grant, and Wes
             Barton, was formed with the goal of creating a unique sound that
-            blends their diverse range of musical influences. Drawing from a
-            wide variety of genres, Pink Ranger has crafted a sound that is both
-            fresh and familiar.
+            blends their diverse range of musical influences.
             <br />
             <br />
             The band is excited to share their music with the world and is
-            currently working on recording their debut EP, which will showcase
-            their energetic and infectious sound.
+            currently working on recording their debut EP.
             <br />
             <br />
           </p>
