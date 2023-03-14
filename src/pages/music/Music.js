a8095cd4f4cd8299b98f4095cb400cd82ad2495f
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2023-03-14 16:07:09 -0600

    Still need styling for moblie

--- src/pages/music/Music.js
+++ src/pages/music/Music.js
@@ -11,12 +11,17 @@ export default function Music() {
     <div className="music">
       <div>
         <h1 className="music-header">Music</h1>
+        <div className="albumart">
         <Link to="https://soundcloud.com/user-639286315/im-tired-demo/s-RCcpom7QyxI?si=74bea2bfdae34713bebe8f5770457f7b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
         <img src={tired} alt="tired" />
         </Link>
         <Link to="https://soundcloud.com/user-639286315/are-u-having-fun-demo/s-x6c8Toh3H8X?si=74bea2bfdae34713bebe8f5770457f7b&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
         <img src={fun} alt="tired" />
         </Link>
+
+
+        </div>
+        
       </div>
     </div>
   );
